3625dc1932c504a8beec1120189a83ac1e497e7b
leonardmouillet <leonard.mouillet@mines-paristech.fr>
2021-01-07 17:50:45 +0100

    liste doublement chaînée, version finale avec makefile

--- liste doublement chaînée/test.cpp
+++ liste doublement chaînée/test.cpp
@@ -115,29 +115,3 @@ void test_print(){
     l.remove_back();
     l.print();
 }
-
-int main (int argn, char* argv []){
-    if (argn > 1){ // n'importe quel caractère passé en argument permet de tester la fonction print
-        std::cout<<"test de la fonction print"<<std::endl;
-        test_print();
-    }
-    else{ // en l'absence d'argument, on teste toutes les autres fonctions
-        std::cout<<"test du constructeur"<<std::endl;
-        test_list_constructor();
-        std::cout<<"test de la fonction add_front"<<std::endl;
-        test_add_front();
-        std::cout<<"test de la fonction add_back"<<std::endl;
-        test_add_back();
-        std::cout<<"test de la fonction is_empty"<<std::endl;
-        test_is_empty();
-        std::cout<<"test de la fonction search"<<std::endl;
-        test_search();
-        std::cout<<"test de la fonction remove_front"<<std::endl;
-        test_remove_front();
-        std::cout<<"test de la fonction remove_back"<<std::endl;
-        test_remove_back();
-        std::cout<<"test de la fonction remove"<<std::endl;
-        test_remove();
-    }
-    return 0;
-}
---
+++ liste doublement chaînée/test.h
@@ -0,0 +1,11 @@
+#include "intlist.h"
+
+void test_list_constructor();
+void test_add_front();
+void test_add_back();
+void test_is_empty();
+void test_remove_front();
+void test_remove_back();
+void test_search();
+void test_remove();
+void test_print();
