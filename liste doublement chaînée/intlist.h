95b8812e0944c0c82ae9c911890b5c8330caee18
leonardmouillet <leonard.mouillet@mines-paristech.fr>
2021-01-07 15:53:10 +0100

    tp liste doublement chaînée, rendu final

--- liste doublement chaînée/intlist.h
+++ liste doublement chaînée/intlist.h
@@ -1,13 +1,15 @@
 #include<iostream>
 
 class IntCell { 
+
     friend class IntList;
+
 private:
-    IntCell (int i) {
-        value = i;
-        previous_cell = 0;
-        next_cell = 0;
-    }  
+
+    IntCell (int i) : 
+    value (i), 
+    previous_cell (nullptr), 
+    next_cell (nullptr) {}
 
     int value;
     IntCell* previous_cell;
@@ -20,68 +22,124 @@ class IntList {
 public:
 
     IntList () {
-        head = 0;
-        tail = 0;
+        head = nullptr;
+        tail = nullptr;
     }
 
     void add_front (int i) {
-        IntCell new_cell (i);
-        if (head == 0) {
-            head = &new_cell;
-            tail = &new_cell;
+        IntCell* new_cell = new IntCell (i);
+        if (head == nullptr) {
+            head = new_cell;
+            tail = new_cell;
         }
         else {
-            new_cell.next_cell = head;
-            (*head).previous_cell = &new_cell;
-            head = &new_cell;
+            (*new_cell).next_cell = head;
+            (*head).previous_cell = new_cell;
+            head = new_cell;
         }
     }
    
     void add_back (int i) {
-        IntCell new_cell (i);
-        if (tail == 0) {
-            head = &new_cell;
-            tail = &new_cell;
+        IntCell* new_cell = new IntCell (i);
+        if (tail == nullptr) {
+            head = new_cell;
+            tail = new_cell;
         }
         else {
-            new_cell.previous_cell = tail;
-            (*head).next_cell = &new_cell;
-            tail = &new_cell;
+            (*new_cell).previous_cell = tail;
+            (*tail).next_cell = new_cell;
+            tail = new_cell;
         }
     }
    
     void remove_front () {
-        if (head != 0) {
+        if (head != nullptr) {
+            IntCell* past_head = head;
             head = (*head).next_cell;
-            (*head.previous_cell) = 0;           
+            if (head != nullptr) {
+                (*head).previous_cell = nullptr;
+            }
+            else {
+                tail = nullptr;
+            }
+            delete past_head;           
         }
         else {
-            throw ("la file est vide")
+            throw ("la file est vide");
         }
     }
    
-   void remove_back () {
-        if (tail != 0) {
+    void remove_back () {
+        if (tail != nullptr) {
+            IntCell* past_tail = tail;
             tail = (*tail).previous_cell;
-            (*tail.next_cell)=0;            
+            if (tail != nullptr){
+                (*tail).next_cell=nullptr;
+            }
+            else{
+                head = nullptr;
+            }
+            delete past_tail;            
         }
         else {
-            throw ("la file est vide")
+            throw ("la file est vide");
         }
     }
    
-   void print () {
-       std::cout<<"[";
-       IntCell* top = head;
-       while (top != 0){
-           std::cout<<(*top).value<<" ";
-           top = (*top).next_cell;
+    void print () {
+        std::cout<<"[";
+        IntCell* top = head;
+        while (top != nullptr){
+            std::cout<<(*top).value<<" ";
+            top = (*top).next_cell;
         }
         std::cout<< "]" << std::endl;
     }
 
-private:
+    bool search (int i) {
+        IntCell* top = head;
+        bool result = false;
+        while (top != nullptr and result !=true) {
+            result = ( (*top).value == i );
+            top = (*top).next_cell;
+        } 
+        return result;
+    }
 
+    bool is_empty() {
+        return head==nullptr;
+    }
+
+    void remove(int i) {
+        IntCell* top = head;
+        bool test = false;
+        while (top != nullptr and test !=true) {
+            test = ( (*top).value == i );
+            if (test == true) {
+                if ((*top).previous_cell != nullptr){
+                    (*((*top).previous_cell)).next_cell=(*top).next_cell;
+                }
+                else {
+                    head = (*top).next_cell;
+                }
+                if((*top).next_cell != nullptr){
+                    (*((*top).next_cell)).previous_cell=(*top).previous_cell;
+                }
+                else {
+                    tail = (*top).previous_cell;
+                }
+                delete top ;
+            }
+            top = (*top).next_cell;
+        }
+        if (test==false){
+            throw ("pas de tel élément dans la liste");
+        }         
+    }
+
+
+
+private:
    IntCell* head;
    IntCell* tail;
 
--- liste doublement chaînée/test.cpp
+++ liste doublement chaînée/test.cpp
@@ -2,28 +2,142 @@
 
 void test_list_constructor(){
     IntList l ;
-    std::cout << std::boolalpha << true << std::endl;
+    std::cout << l.is_empty() << std::endl;
 }
 
 void test_add_front(){
     IntList l;
     l.add_front (10);
-    //l.add_front (20);
-    l.print();
-    std::cout << std::boolalpha << true << std::endl;
+    std::cout << l.search(10);
+    l.add_front (20);
+    std::cout << l.search(10);
+    std::cout << l.search(20) << std::endl;
 }
 
-int test_add_back(){
+void test_add_back(){
     IntList l;
-    //l.add_back (10);
-    //l.add_back (20);
-    l.print();
-    std::cout << std::boolalpha << true << std::endl;
+    l.add_back (10);
+    std::cout << l.search(10);
+    l.add_back (20);
+    std::cout << l.search(10);
+    std::cout << l.search(20) << std::endl;
 }
 
-int main (){
-    test_list_constructor();
-    test_add_front();
-    test_add_back();
+void test_is_empty(){
+    IntList l ;
+    std::cout << l.is_empty();
+    l.add_back(20);
+    std::cout << !(l.is_empty()) << std::endl;
+}
+
+
+
+void test_remove_front(){
+    IntList l;
+    l.add_back(10);
+    l.add_back(20);
+    l.remove_front();
+    std::cout << !(l.search(10));
+    std::cout << l.search(20);
+    l.remove_front();
+    std::cout << !(l.search(20));
+    std::cout << l.is_empty();
+    try {
+        l.remove_front();
+    }
+    catch (const char* e) {
+            std::cout << true << std::endl;
+    }
+}
+
+void test_remove_back(){
+    IntList l;
+    l.add_back(10);
+    l.add_back(20);
+    l.remove_back();
+    std::cout << !(l.search(20));
+    std::cout << l.search(10);
+    l.remove_back();
+    std::cout << !(l.search(10));
+    std::cout << l.is_empty();
+    try {
+        l.remove_back();
+    }
+    catch (const char* e) {
+        std::cout << true << std::endl;
+    }
+}
+
+void test_search(){
+    IntList l;
+    l.add_front(10);
+    l.add_front(20);
+    std::cout<<l.search(10);
+    std::cout<<!(l.search(30))<<std::endl;
+}
+
+void test_remove(){
+    IntList l;
+    try{
+        l.remove(10);
+    }
+    catch (const char* e) {
+        std::cout << true;
+    }
+    l.add_front(10);
+    l.add_front(20);
+    l.remove(10);
+    std::cout << l.search(20);
+    std::cout << !(l.search(10));
+    try{
+        l.remove(10);
+    }
+    catch (const char* e) {
+        std::cout << true;
+    }  
+    l.remove(20);
+    std::cout << l.is_empty() << std::endl;
+}
+
+void test_print(){
+    IntList l;
+    l.print();
+    l.add_back(1);
+    l.print();
+    l.add_back(2);
+    l.print();
+    l.add_back(3);
+    l.print();
+    l.remove_back();
+    l.print();
+    l.remove_back();
+    l.print();
+    l.remove_back();
+    l.print();
+}
+
+int main (int argn, char* argv []){
+    if (argn > 1){ // n'importe quel caractère passé en argument permet de tester la fonction print
+        std::cout<<"test de la fonction print"<<std::endl;
+        test_print();
+    }
+    else{ // en l'absence d'argument, on teste toutes les autres fonctions
+        std::cout<<"test du constructeur"<<std::endl;
+        test_list_constructor();
+        std::cout<<"test de la fonction add_front"<<std::endl;
+        test_add_front();
+        std::cout<<"test de la fonction add_back"<<std::endl;
+        test_add_back();
+        std::cout<<"test de la fonction is_empty"<<std::endl;
+        test_is_empty();
+        std::cout<<"test de la fonction search"<<std::endl;
+        test_search();
+        std::cout<<"test de la fonction remove_front"<<std::endl;
+        test_remove_front();
+        std::cout<<"test de la fonction remove_back"<<std::endl;
+        test_remove_back();
+        std::cout<<"test de la fonction remove"<<std::endl;
+        test_remove();
+    }
     return 0;
 }
